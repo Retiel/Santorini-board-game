a916617fc661c6a1ad571df8c3a54fddfacb687a
Filippo <fax.95@hotmail.it>
2020-03-26 15:08:52 +0100

    Controller can now manage turns.

---
+++ src/main/java/controller/TurnManagerInterface.java
@@ -0,0 +1,16 @@
+package controller;
+
+/**
+ * Interface for TurnManager
+ */
+public interface TurnManagerInterface {
+
+    /**
+     *
+     */
+    void setStartingPlayer();
+
+    void nextPlayer();
+
+    void nextPhase();
+}
