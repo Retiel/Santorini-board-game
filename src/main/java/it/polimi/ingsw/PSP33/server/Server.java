fa3a2f4a7da13dff8280a57c02690eb56199454f
Filippo <fax.95@hotmail.it>
2020-07-02 17:07:39 +0200

    Server now can exit.

--- src/main/java/it/polimi/ingsw/PSP33/server/Server.java
+++ src/main/java/it/polimi/ingsw/PSP33/server/Server.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.PSP33.server;
 
+import it.polimi.ingsw.PSP33.view.gui.SetupFrame;
+
+import javax.swing.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,6 +23,7 @@ public class Server {
     public final static int SOCKET_PORT = 7777;
 
     public static void main(String[] args) {
+        System.out.println("Server starting..");
         try {
             //Open server's socket
             serverSocket = new ServerSocket(SOCKET_PORT);
@@ -27,6 +31,9 @@ public class Server {
             System.out.println("Cannot open server socket");
             System.exit(1);
         }
+        System.out.println("Server ready");
+
+        SwingUtilities.invokeLater(Server::showServerWindow);
 
         while(true) {
             try {
@@ -40,6 +47,16 @@ public class Server {
                 e.printStackTrace();
                 break;
             }
+        }
+    }
+
+    public static void showServerWindow() {
+        JOptionPane optionPane = new JOptionPane("Server is running\nPress OK to close the server",
+                JOptionPane.PLAIN_MESSAGE,
+                JOptionPane.DEFAULT_OPTION);
+        SetupFrame.lockClosure(null, optionPane);
+        if((int) optionPane.getValue() == 0) {
+            System.exit(0);
         }
     }
 }
